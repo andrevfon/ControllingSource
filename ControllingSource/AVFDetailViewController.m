72dcf5e75082810c73b4852c9e55747b94d877f4
andre <andrevfon@yahoo.com.br>
2014-07-29 17:38:33 -0300

    mudei a cor da DetailViewControl  para vermelho

--- ControllingSource/AVFDetailViewController.m
+++ ControllingSource/AVFDetailViewController.m
@@ -40,6 +40,8 @@
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
     [self configureView];
+    
+    self.view.backgroundColor = [UIColor redColor];
 }
 
 - (void)didReceiveMemoryWarning
